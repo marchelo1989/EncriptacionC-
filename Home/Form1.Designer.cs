d34afc1c7005730b44681b575b13bbc814c39dab
march <march@TOSHIBA>
2017-09-05 18:54:04 -0300

    Agregar archivos de proyecto.

---
+++ Home/Form1.Designer.cs
@@ -0,0 +1,130 @@
+﻿namespace Home
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtMd5 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.lblMensaje = new System.Windows.Forms.ListBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtSha1 = new System.Windows.Forms.TextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // txtMd5
+            // 
+            this.txtMd5.Location = new System.Drawing.Point(138, 34);
+            this.txtMd5.Name = "txtMd5";
+            this.txtMd5.Size = new System.Drawing.Size(100, 20);
+            this.txtMd5.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(31, 37);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(93, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Encriptar en MD5:";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(255, 32);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(111, 23);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Encriptar &MD5";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // lblMensaje
+            // 
+            this.lblMensaje.FormattingEnabled = true;
+            this.lblMensaje.Location = new System.Drawing.Point(34, 194);
+            this.lblMensaje.Name = "lblMensaje";
+            this.lblMensaje.Size = new System.Drawing.Size(369, 43);
+            this.lblMensaje.TabIndex = 4;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(28, 67);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(98, 13);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Encriptar en SHA1:";
+            // 
+            // txtSha1
+            // 
+            this.txtSha1.Location = new System.Drawing.Point(138, 64);
+            this.txtSha1.Name = "txtSha1";
+            this.txtSha1.Size = new System.Drawing.Size(100, 20);
+            this.txtSha1.TabIndex = 6;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(255, 62);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(111, 23);
+            this.button2.TabIndex = 7;
+            this.button2.Text = "Encriptar &SHA1";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(502, 261);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.txtSha1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.lblMensaje);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtMd5);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtMd5;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ListBox lblMensaje;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtSha1;
+        private System.Windows.Forms.Button button2;
+    }
+}
+
---
+++ Home/Form1.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+using Encriptacion;
+namespace Home
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            txtMd5.Text = "ABCabc123";
+            txtSha1.Text = "ABCabc123";
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            MD5 md5 = new MD5();
+            string texto = txtMd5.Text;
+            md5.EncripatarMD5(texto);
+            lblMensaje.Items.Add(md5.EncripatarMD5(texto));
+            //lMensaje.Items.Clear();
+            txtSha1.Text= md5.EncripatarMD5(texto);
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            SHA1_1 sha1 = new SHA1_1();
+            string texto = txtSha1.Text;
+            sha1.GetSHA1(texto);
+            lblMensaje.Items.Add(sha1.GetSHA1(texto));
+            //lblMensaje.Items.Clear();
+        }
+    }
+}
