8cf810be59a3664b345cabe813b6eace7506aebf
march <march@DESKTOP-C7HETK4>
2019-02-11 01:25:07 -0300

    Implemntacion AES- BASE64 y CommonsCodec19 y cambios en la Inter GUI

--- Home/Form1.cs
+++ Home/Form1.cs
@@ -9,6 +9,7 @@ using System.Threading.Tasks;
 using System.Windows.Forms;
 
 using Encriptacion;
+
 namespace Home
 {
     public partial class Form1 : Form
@@ -16,8 +17,12 @@ namespace Home
         public Form1()
         {
             InitializeComponent();
-            txtMd5.Text = "ABCabc123";
-            txtSha1.Text = "ABCabc123";
+            string dato = "Hola Mundo";
+            txtMd5.Text = dato;
+            txtSha1.Text = dato;
+            txtEncripBase64.Text = dato;
+            txtEncripAES.Text = dato;
+            txtEncripCode.Text = dato;
         }
 
         private void button1_Click(object sender, EventArgs e)
@@ -27,7 +32,7 @@ namespace Home
             md5.EncripatarMD5(texto);
             lblMensaje.Items.Add(md5.EncripatarMD5(texto));
             //lMensaje.Items.Clear();
-            txtSha1.Text= md5.EncripatarMD5(texto);
+            //txtSha1.Text= md5.EncripatarMD5(texto);
         }
 
         private void button2_Click(object sender, EventArgs e)
@@ -36,6 +41,63 @@ namespace Home
             string texto = txtSha1.Text;
             sha1.GetSHA1(texto);
             lblMensaje.Items.Add(sha1.GetSHA1(texto));
+            //lblMensaje.Items.Clear();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            BASE64 base64 = new BASE64();
+            string texto = txtEncripBase64.Text;
+            base64.Encriptar(texto);
+            lblMensaje.Items.Add(base64.Encriptar(texto));
+            txtDesenBase64.Text = base64.Encriptar(texto);
+            //lblMensaje.Items.Clear();
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            BASE64 base64 = new BASE64();
+            string texto = txtDesenBase64.Text;
+            base64.DesEncriptar(texto);
+            lblMensaje.Items.Add(base64.DesEncriptar(texto));
+            //lblMensaje.Items.Clear();
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            AESEN aes = new AESEN();
+            string texto = txtEncripAES.Text;
+            aes.Encrit(texto);
+            lblMensaje.Items.Add(aes.Encrit(texto));
+            //lblMensaje.Items.Clear();
+            txtDesmAES.Text = aes.Encrit(texto);
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            AESEN aes = new AESEN();
+            string texto = txtDesmAES.Text;
+            aes.Desencrit(texto);
+            lblMensaje.Items.Add(aes.Desencrit(texto));
+            //lblMensaje.Items.Clear();
+        }
+
+        private void button7_Click(object sender, EventArgs e)
+        {
+            CommonsCodec19 comm = new CommonsCodec19();
+            string texto = txtEncripCode.Text;
+            comm.Encriptar(texto);
+            lblMensaje.Items.Add(comm.Encriptar(texto));
+            //lblMensaje.Items.Clear();
+            txtDenseCode.Text = comm.Encriptar(texto);
+        }
+
+        private void button8_Click(object sender, EventArgs e)
+        {
+            CommonsCodec19 comm = new CommonsCodec19();
+            string texto = txtDenseCode.Text;
+            comm.Desencriptar(texto);
+            lblMensaje.Items.Add(comm.Desencriptar(texto));
             //lblMensaje.Items.Clear();
         }
     }
